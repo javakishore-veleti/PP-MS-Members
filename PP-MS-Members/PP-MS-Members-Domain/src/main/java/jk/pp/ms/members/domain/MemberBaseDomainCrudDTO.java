c4bbd063c81a7bac5016914b3d8c94bf7a5256b1
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-23 17:21:33 -0400

    Refactored the code

--- PP-MS-Members/PP-MS-Members-Domain/src/main/java/jk/pp/ms/members/domain/MemberBaseDomainCrudDTO.java
+++ PP-MS-Members/PP-MS-Members-Domain/src/main/java/jk/pp/ms/members/domain/MemberBaseDomainCrudDTO.java
@@ -1,6 +1,6 @@
 package jk.pp.ms.members.domain;
 
-import jk.pp.engg.foundations.common.core.dto.DomainCrudDTO;
+import jk.pp.engg.foundations.common.domain.core.DomainCrudDTO;
 
 public class MemberBaseDomainCrudDTO extends DomainCrudDTO<MemberBaseDomain> {
 
