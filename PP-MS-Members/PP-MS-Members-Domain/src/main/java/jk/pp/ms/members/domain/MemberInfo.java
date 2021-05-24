2a0ef54acb98956184e0be40e054315b02a8a44a
KISHORE VELETI <vavkkishore@kishores-mbp.home>
2021-05-24 15:55:08 -0400

    Apache Kafka initial commit

--- PP-MS-Members/PP-MS-Members-Domain/src/main/java/jk/pp/ms/members/domain/MemberInfo.java
+++ PP-MS-Members/PP-MS-Members-Domain/src/main/java/jk/pp/ms/members/domain/MemberInfo.java
@@ -1,5 +1,6 @@
 package jk.pp.ms.members.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -16,5 +17,17 @@ import lombok.NoArgsConstructor;
 public class MemberInfo extends BaseDomain {
 
 	private static final long serialVersionUID = 1L;
+
+	@Column(name = "first_name", length = 30)
+	private String firstName;
+
+	@Column(name = "last_name", length = 30)
+	private String lastName;
+
+	@Column(name = "email", length = 50)
+	private String email;
+
+	@Column(name = "username", length = 20)
+	private String userName;
 
 }
--- PP-MS-Members/PP-MS-Members-Web/src/main/java/jk/pp/ms/members/web/controller/MemberInfoCrudController.java
+++ PP-MS-Members/PP-MS-Members-Web/src/main/java/jk/pp/ms/members/web/controller/MemberInfoCrudController.java
@@ -1,5 +1,9 @@
 package jk.pp.ms.members.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,44 @@ public class MemberInfoCrudController extends AppCrudController<MemberInfo, Memb
 	@Autowired
 	public MemberInfoCrudController(MemberInfoCrudService service) {
 		super.setCrudService(service);
+	}
+
+	@Override
+	public List<MemberInfoCrudDTO> populateInitialDataDomais() {
+
+		List<MemberInfoCrudDTO> initialDataDomains = new ArrayList<MemberInfoCrudDTO>();
+
+		MemberInfoCrudDTO crudDTO = null;
+		MemberInfo asset = null;
+
+		int length = 25;
+		boolean useLetters = true;
+		boolean useNumbers = false;
+		String generatedString = null;
+
+		for (int ctr = 1; ctr <= 10; ++ctr) {
+			crudDTO = new MemberInfoCrudDTO();
+
+			asset = new MemberInfo();
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+
+			asset.setFirstName(generatedString);
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+			asset.setLastName(generatedString);
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+			asset.setEmail(generatedString);
+
+			generatedString = RandomStringUtils.random(length, useLetters, useNumbers);
+			asset.setUserName(generatedString);
+			
+			crudDTO.setDomain(asset);
+
+			initialDataDomains.add(crudDTO);
+		}
+
+		return initialDataDomains;
 	}
 }
